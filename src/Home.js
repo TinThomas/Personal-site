91457461572492621c786ff3a312786fdceb8b5f
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-03-21 23:31:36 +0100

    Added about me and portfolio buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
   return (
     <div className="homePage">
-      <h1 className="pageHeader">Home page</h1>
+      <h1 className="pageHeader">Thomas Bærentsen</h1>
+      <div className="homeNavButtons">
+        <button className="navButton">About me</button>
+        <button className="navButton">Portfolio</button>
+      </div>
+
     </div>
   );
 }
