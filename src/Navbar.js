906a3c409f3486f6df6a2f993be6db46afb5e51d
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-03-29 22:00:25 +0200

    Added portfolio dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,13 @@ const Navbar = () => {
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/pending-construction">About me</Link>
-        <Link to="/pending-construction">Portfolio</Link>
+        <Link className="dropdown" to="/pending-construction">Portfolio
+          <div className="dropdown-content">
+            <p>Item one</p>
+            <p>Item two</p>
+          </div>
+        </Link>
+
         <Link to="/pending-construction">Contact</Link>
       </div>
     </nav>
