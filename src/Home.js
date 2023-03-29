1f4f99694c02135c29d8d64ccbf567dcf47b4939
au554955 <201606351@post.au.dk>
2023-03-29 09:34:37 +0200

    Some reformatting

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import profilePicture from "./Images/Profile-picture.jpg"
 
 const Home = () => {
   return (
     <div className="homePage">
-      <h1 className="pageHeader">Thomas Bærentsen</h1>
-      <div className="homeNavButtons">
-        <Link to="/pending-construction">
-          <button className="navButton">About me</button>
-        </Link>
-        <Link to="/pending-construction">
-          <button className="navButton">Portfolio</button>
-        </Link>
-      </div>
+
+      <section className="homePageContent">
+        <div className="homeNavButtons">
+          <Link to="/pending-construction">
+            <button className="navButton">About me</button>
+          </Link>
+          <Link to="/pending-construction">
+            <button className="navButton">Portfolio</button>
+          </Link>
+        </div>
+
+        <img className="homePageImage" src={profilePicture} alt="Me">
+
+        </img>
+      </section>
 
     </div>
   );
