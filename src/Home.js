234fd459481d8778ad5ffc1ba919f6d53fca7b33
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-04-01 10:15:36 +0200

    Added the smaller profile image

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import profilePicture from "./Images/Profile-picture.jpg"
+import profilePicture from "./Images/Profile-picture-small.jpg"
 
 const Home = () => {
   return (
