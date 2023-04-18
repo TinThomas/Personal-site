e5e6caa304872a66b85bef9cacb806b95a5917e6
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-04-18 22:32:50 +0200

    Added a bit of basic stuff

--- src/About.js
+++ src/About.js
@@ -3,8 +3,11 @@ import profilePicture from "./Images/Profile-picture-small.jpg"
 const About = () => {
   return (
     <div className="About">
-      <div className="About-contents">Content goes here</div>
-      <img src={profilePicture} alt="Profile" />
+      <div className="About-contents">
+        <p>Thing number one</p>
+        <p>Thing number two</p>
+      </div>
+      <img className="About-image" src={profilePicture} alt="Profile" />
     </div>
   );
 }
