21a211086d861032f7f5b1fe36dff32d4c9a2a7c
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-04-20 22:15:59 +0200

    Added intro paragraph and content json file

--- src/About.js
+++ src/About.js
@@ -1,11 +1,11 @@
 import profilePicture from "./Images/Profile-picture-small.jpg"
+import content from './Content/content_en.json'
 
 const About = () => {
   return (
     <div className="About">
       <div className="About-contents">
-        <p>Thing number one</p>
-        <p>Thing number two</p>
+        <p>{content.intro_paragraph}</p>
       </div>
       <img className="About-image" src={profilePicture} alt="Profile" />
     </div>
