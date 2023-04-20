24c9c82e8d26743317538ace0b4add686cfcab76
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-04-20 22:33:35 +0200

    Split the intro paragraph in two to add line break

--- src/About.js
+++ src/About.js
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <div className="About">
       <div className="About-contents">
-        <p>{content.intro_paragraph}</p>
+        <p>{content.intro_paragraph_1}</p>
+        <br />
+        <p>{content.intro_paragraph_2}</p>
       </div>
       <img className="About-image" src={profilePicture} alt="Profile" />
     </div>
