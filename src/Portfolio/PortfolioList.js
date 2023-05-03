fa8b88a6c4057c429d5d02aa426ade5fa95ab2ef
au554955 <201606351@post.au.dk>
2023-05-03 10:19:53 +0200

    Adde spaceship to portfolio list

--- src/Portfolio/PortfolioList.js
+++ src/Portfolio/PortfolioList.js
@@ -1,6 +1,14 @@
+import content from '../Content/content_en.json'
+import Spaceship_image from '../Images/Portfolio/Spaceship/Spaceship_front_led.jpg'
+
 const PortfolioList = () => {
   return (
-    <div className="portfolioList"></div>
+    <div className="portfolioList">
+      <section>
+        <p>{content.spaceship_title}</p>
+        <img src={Spaceship_image}></img>
+      </section>
+    </div>
   );
 }
 
