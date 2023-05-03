0cf2cf71b9ca9fde3078b51b7c3ff777e99aca3a
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-05-03 20:09:56 +0200

    Turned section in portfolioList into Links

--- src/Portfolio/PortfolioList.js
+++ src/Portfolio/PortfolioList.js
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import content from '../Content/content_en.json'
 import Spaceship_image from '../Images/Portfolio/Spaceship/Spaceship_front_led.jpg'
 
 const PortfolioList = () => {
   return (
     <div className="portfolioList">
-      <section className='frosted'>
+      <Link to='/Spaceship' className='frosted'>
         <p>{content.spaceship_title}</p>
         <img src={Spaceship_image} alt='The spaceship toy from the front, with lit up LEDs'></img>
-      </section>
+      </Link>
     </div>
   );
 }
