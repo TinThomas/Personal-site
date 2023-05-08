e7d4340cfd0f74a7e9eed1f1fb4ef7e16ce53884
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-05-09 00:47:48 +0200

    Added BMS section to KLS

--- src/Portfolio/KLS.js
+++ src/Portfolio/KLS.js
@@ -3,11 +3,13 @@ import PortfolioBlockRight from "./PortfolioBlockRight";
 import PortfolioBlockLeft from './PortfolioBlockLeft';
 
 import Final_prototype from '../Images/Portfolio/KLS/Final_prototype.jpg'
+import bms from '../Images/Portfolio/KLS/bms.png'
 
 const KLS = () => {
   return (
   <div className="portfolioPage">
     <PortfolioBlockRight text={content.KLS_paragraph_1} img={Final_prototype}/>
+    <PortfolioBlockLeft text={content.KLS_paragraph_2} img={bms}/>
   </div>
 );
 }
