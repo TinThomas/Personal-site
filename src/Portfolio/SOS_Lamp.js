43b966cf0e3b40fa85420854e57ed8d793e4958c
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-05-10 22:57:39 +0200

    Added SOS Lamp descriptions

--- src/Portfolio/SOS_Lamp.js
+++ src/Portfolio/SOS_Lamp.js
@@ -3,12 +3,15 @@ import PortfolioBlockRight from "./Components/PortfolioBlockRight";
 import PortfolioBlockLeft from './Components/PortfolioBlockLeft';
 
 import full from '../Images/Portfolio/SOS/Lamp_full_below.jpg'
+import motion from '../Images/Portfolio/SOS/Lamp-fixed.png'
+import servos from '../Images/Portfolio/SOS/Lamp_servos.jpg'
 
 const SOS_Lamp = () => {
   return (
     <div className="portfolioPage">
       <PortfolioBlockRight text={content.SOS_paragraph_1} img={full}/>
-      <PortfolioBlockLeft text={content.SOS_paragraph_2} img={full}/>
+      <PortfolioBlockLeft text={content.SOS_paragraph_2} img={motion}/>
+      <PortfolioBlockRight text={content.SOS_paragraph_3} img={servos}/>
     </div>
   );
 }
