260a24bda47b02318f75eeaa9621f8a82c67e54b
au554955 <201606351@post.au.dk>
2023-05-14 15:18:10 +0200

    Horizontal scrollbar now no longer appears

--- src/Portfolio/SOS_Lamp.js
+++ src/Portfolio/SOS_Lamp.js
@@ -12,6 +12,9 @@ const SOS_Lamp = () => {
       <PortfolioBlockRight text={content.SOS_paragraph_1} img={full}/>
       <PortfolioBlockLeft text={content.SOS_paragraph_2} img={motion}/>
       <PortfolioBlockRight text={content.SOS_paragraph_3} img={servos}/>
+      <PortfolioBlockRight text={content.SOS_paragraph_3} img={servos}/>
+      <PortfolioBlockRight text={content.SOS_paragraph_3} img={servos}/>
+      <PortfolioBlockRight text={content.SOS_paragraph_3} img={servos}/>
     </div>
   );
 }
