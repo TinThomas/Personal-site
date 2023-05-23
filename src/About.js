3e88dc2a4849176e9fc1a5d94866f34a8e86ffb0
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-05-23 23:02:17 +0200

    Added frosted effect to about page

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ import content from './Content/content_en.json'
 const About = () => {
   return (
     <div className="About">
-      <div className="About-contents">
+      <div className="About-contents frosted">
         <p>{content.intro_paragraph_1}</p>
         <br />
         <p>{content.intro_paragraph_2}</p>
