d13042472dac5c4128aa02931ab4fed26c6e912b
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-06-07 16:44:01 +0200

    Workflow section

--- src/About.js
+++ src/About.js
@@ -10,9 +10,9 @@ const About = () => {
 
         <AboutBlock title={content.intro_paragraph_title} body={content.intro_paragraph_body}></AboutBlock>
 
-        <AboutBlock title={content.Languages_and_frameworks_title} body={content.Languages_and_frameworks_body}></AboutBlock>
-
         <AboutBlock title={content.tools_and_technologies_title} body={content.tools_and_technologies_body}></AboutBlock>
+        <AboutBlock title={content.Languages_and_frameworks_title} body={content.Languages_and_frameworks_body}></AboutBlock>
+        <AboutBlock title={content.work_methods_title} body={content.work_methods_body}></AboutBlock>
 
       </div>
       <img className="About-image" src={profilePicture} alt="Profile" />
