e2f57199de9cf5de55d5400c6e30a58429d6b09d
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-06-09 22:48:39 +0200

    Added some education

--- src/EducationAndWork.js
+++ src/EducationAndWork.js
@@ -1,10 +1,16 @@
 const EducationAndWork = () => {
   return (
     <div className="EducationAndWork">
-      <div className="WorkBlock">
+      <div className="WorkBlock frosted">
         <h2>Education</h2>
+        <h3>2020 - 2023</h3>
+        <p>Bachelor of IT Product Development at Aarhus University</p>
+        <h3>2018 - 2020</h3>
+        <p>Softwaretechnology Engineering at Aarhus University</p>
+        <h3>2016 - 2017</h3>
+        <p>Bachelor of physics at Aarhus University</p>
       </div>
-      <div className="WorkBlock">
+      <div className="WorkBlock frosted">
         <h2>Work</h2>
       </div>
     </div>
