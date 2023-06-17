31735c77f56095ae98140749ba53c539d91df55d
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-06-18 00:00:21 +0200

    Completed Calendar page

--- src/Portfolio/Calendar.js
+++ src/Portfolio/Calendar.js
@@ -3,12 +3,14 @@ import PortfolioBlockRight from "./Components/PortfolioBlockRight";
 import PortfolioBlockLeft from './Components/PortfolioBlockLeft';
 import User from '../Images/Portfolio/Calendar/User.jpg'
 import Prototype from '../Images/Portfolio/Calendar/Prototype.png'
+import Thinkaloud from '../Images/Portfolio/Calendar/ThinkAloud.jpg'
 
 const Calendar = () => {
   return (
     <div className="portfolioPage">
     <PortfolioBlockRight text={content.Calendar_paragraph_1} img={Prototype}/>
     <PortfolioBlockLeft text={content.Calendar_paragraph_2} img={User}/>
+    <PortfolioBlockRight text={content.Calendar_paragraph_3} img={Thinkaloud}/>
     </div>
   );
 }
