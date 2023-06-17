25c9e855648a70ca8731d0329a6de7c23b3abf02
Thomas Bærentsen <thomas.baerentsen@gmail.com>
2023-06-17 17:33:40 +0200

    Added bachelor project description

--- src/Portfolio/Bachelor.js
+++ src/Portfolio/Bachelor.js
@@ -2,12 +2,13 @@ import content from '../Content/content_en.json'
 import PortfolioBlockRight from "./Components/PortfolioBlockRight";
 import PortfolioBlockLeft from './Components/PortfolioBlockLeft';
 import Selection from '../Images/Portfolio/Bachelor/Buying-ticket-confirm.png';
+import Responses from '../Images/Portfolio/Bachelor/Response_by_modality.png'
 
 const Bachelor = () => {
   return (
     <div className="portfolioPage">
     <PortfolioBlockRight text={content.Bachelor_paragraph_1} img={Selection}/>
-    <PortfolioBlockLeft text={content.Bachelor_paragraph_1} img={Selection}/>
+    <PortfolioBlockLeft text={content.Bachelor_paragraph_2} img={Responses}/>
   </div>
   );
 }
